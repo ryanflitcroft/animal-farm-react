7347f00e38ab9d805046d372c1b24861d97084a8
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-01-25 17:45:18 -0800

    function written for Header, Footer

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -1,7 +1,7 @@
 import './Animal.css';
 
 export default function Animal(props) {
-  console.log(props);
+
   return (
     <div style={{ top: `${props.top}`, left: `${props.left}` }}>
       <img src={`../animals/${props.type}.svg`} alt={props.name} height={props.height} width={props.width} />
--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
 import { animals } from './data';
+import Header from './Header/Header';
 import Main from './Main/Main';
+import Footer from './Footer/Footer';
 
 function App() {
   // console.log(animals);
   return (
-    <Main animals={animals} />
+    <>
+      <Header greeting= 'Hello, Welcome to Animal Farm!' />
+      <Main animals={animals} />
+      <Footer email= 'farmer@animalfarm.co' />
+    </>
   );
 }
 
