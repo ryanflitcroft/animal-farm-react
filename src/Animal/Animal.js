25fccb00404a67b9d587707beba2d250658bc415
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-01-25 16:53:00 -0800

    inline styles added for function Animal elements, css styles applied

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -3,7 +3,7 @@ import './Animal.css';
 export default function Animal(props) {
   console.log(props);
   return (
-    <div>
+    <div style={{ top: `${props.top}`, left: `${props.left}` }}>
       <img src={`../animals/${props.type}.svg`} alt={props.name} height={props.height} width={props.width} />
       <p>
         <span>{props.name} says:</span>
